5eb2337de16cbecfea010d5184a29357b2415a59
Ashim Ujjwal <ashim.ujjwal619@gmail.com>
2019-06-07 13:14:52 +0530

    dashboard

--- resources/templates/back/admin_content.php
+++ resources/templates/back/admin_content.php
@@ -8,7 +8,7 @@
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
-                                <i class="fa fa-dashboard"></i> Dashboard
+                                <i class="fa fa-dashboard"></i> Dashboardd
                             </li>
                         </ol>
                     </div>
